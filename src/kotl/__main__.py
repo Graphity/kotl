3d03e5578ccc6ebc70882df1a6bd2487d44c740b
Graphity <rohood3@gmail.com>
2022-07-11 01:36:01 +0400

    Add '--reverse' option

--- src/kotl/__main__.py
+++ src/kotl/__main__.py
@@ -5,13 +5,14 @@ import json
 import os
 import subprocess
 
-from .settings import THEMES_DIR
+from .settings import THEMES_DIR, HOME
 from .utils import json_to_xresources
 
 @click.command()
 @click.argument('theme', required=False)
 @click.option('-l', '--list-themes', is_flag=True, help='List built-in themes.')
-def kotl(theme, list_themes):
+@click.option('--reverse', is_flag=True, help='Reverse changes.')
+def kotl(theme, list_themes, reverse):
     themes = os.listdir(THEMES_DIR)
     if theme:
         theme += '.json'
@@ -24,3 +25,12 @@ def kotl(theme, list_themes):
     elif list_themes:
         for theme in themes:
             print(os.path.splitext(theme)[0])
+    elif reverse:
+        subprocess.run(['xrdb', '-remove'])
+        xresources_paths = [
+            os.path.join(HOME, '.Xresources'),
+            os.path.join(HOME, '.config/x11/xresources')
+        ]
+        for path in xresources_paths:
+            if os.path.exists(path):
+                subprocess.run(['xrdb', path])
